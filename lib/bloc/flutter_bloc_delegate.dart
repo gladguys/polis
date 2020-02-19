88d317c1b68a2171d62625d2feff0d134940567b
Rodrigo Bastos <rodrigobastosv@gmail.com>
2020-02-19 09:57:01 -0300

    Improving the logging

--- lib/bloc/flutter_bloc_delegate.dart
+++ lib/bloc/flutter_bloc_delegate.dart
@@ -4,13 +4,14 @@ class FlutterBlocDelegate extends BlocDelegate {
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
-    print(event);
+    print('[Bloc] Event: $event');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+    print('[Bloc] State Transition: '
+        '[${transition.currentState} => ${transition.nextState}]');
   }
 
   @override
