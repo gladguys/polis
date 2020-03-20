3de9c7e06ed115a2fc4602d611e540a30086947a
rodrigobastosv <rodrigobastosv@gmail.com>
2020-03-20 10:39:47 -0300

    Mapeando a exceção

--- lib/core/exception/exceptions.dart
+++ lib/core/exception/exceptions.dart
@@ -3,5 +3,6 @@ export 'email_already_in_use_exception.dart';
 export 'google_signin_exception.dart';
 export 'invalid_credentials_exception.dart';
 export 'signout_exception.dart';
+export 'upload_file_exception.dart';
 export 'user_deleted_exception.dart';
 export 'weak_password_exception.dart';
