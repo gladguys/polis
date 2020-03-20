86de17a043b1f8f1797f25de3b233a44e00678ab
rodrigobastosv <rodrigobastosv@gmail.com>
2020-03-20 16:45:18 -0300

    Permitindo const constructor

--- lib/widget/centered_loading.dart
+++ lib/widget/centered_loading.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CenteredLoading extends StatelessWidget {
+  const CenteredLoading();
+
   @override
   Widget build(BuildContext context) {
     return const Center(
