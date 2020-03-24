24d23ae94140e4a949b2104f5f56ad461efb9f9e
rodrigobastosv <rodrigobastosv@gmail.com>
2020-03-24 12:05:55 -0300

    Creating DI for repositoriea

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:polis/widget/my_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'bloc/flutter_bloc_delegate.dart';
 import 'core/service/ad_service.dart';
 import 'core/service/locator.dart';
 import 'core/service/services.dart';
-import 'widget/my_app.dart';
+import 'widget/my_app_injections.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +22,9 @@ void main() async {
 
   runZoned(() {
     runApp(
-      MyApp(),
+      MyAppInjections(
+        child: MyApp(),
+      ),
     );
   }, onError: G<CrashlyticsService>().crashlytics.recordError);
 }
---
+++ lib/widget/my_app_injections.dart
@@ -0,0 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../repository/concrete/firebase/firebase_follow_repository.dart';
+import '../repository/concrete/firebase/firebase_user_profile_repository.dart';
+import '../repository/concrete/repositories.dart';
+
+const SCOPE_TYPE = 'email';
+const SCOPE_URL = 'https://www.googleapis.com/auth/contacts.readonly';
+
+class MyAppInjections extends StatelessWidget {
+  MyAppInjections({this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (_) => FirebaseSigninRepository(
+            firebaseAuth: FirebaseAuth.instance,
+            firestore: Firestore.instance,
+            googleSignin: GoogleSignIn(
+              scopes: [
+                SCOPE_TYPE,
+                SCOPE_URL,
+              ],
+            ),
+          ),
+        ),
+        RepositoryProvider(
+          create: (_) => FirebaseSignupRepository(
+            firebaseAuth: FirebaseAuth.instance,
+            firestore: Firestore.instance,
+            storage: FirebaseStorage.instance,
+          ),
+        ),
+        RepositoryProvider(
+          create: (_) => FirebaseUserFollowingPoliticsRepository(
+            firestore: Firestore.instance,
+          ),
+        ),
+        RepositoryProvider(
+          create: (_) => FirebaseUserProfileRepository(
+            firestore: Firestore.instance,
+          ),
+        ),
+        RepositoryProvider(
+          create: (_) => FirebaseUserRepository(
+            firestore: Firestore.instance,
+            firebaseAuth: FirebaseAuth.instance,
+          ),
+        ),
+        RepositoryProvider(
+          create: (_) => FirebaseFollowRepository(
+            firestore: Firestore.instance,
+          ),
+        ),
+        RepositoryProvider(
+          create: (_) => UserInfoRepositoryImpl(
+            geolocator: Geolocator(),
+            geocoding: Geocoder.local,
+          ),
+        ),
+        RepositoryProvider(
+          create: (_) => FirebasePoliticSuggestionRepository(
+            firestore: Firestore.instance,
+            userInfoRepository: _.repository<UserInfoRepositoryImpl>(),
+          ),
+        ),
+      ],
+      child: child,
+    );
+  }
+}
