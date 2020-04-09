2871526252c59913d8daaac0035c4cd3ac1b0c58
denis <dineresc@gmail.com>
2020-04-09 01:10:14 -0300

    tirando function que nao existe

--- functions/index.js
+++ functions/index.js
@@ -223,38 +223,3 @@ exports.onCreateActivity = functions.firestore
                 }
             });
     })
-
-
-exports.onCreateActivity = functions.firestore
-    .document('/atividades/{politicoId}/atividadesPolitico/{documentId}')
-    .onCreate(async (snapshot, context) => {
-        const politicoId = context.params.politicoId;
-        const documentId = context.params.documentId;
-
-        const timelineRef = admin
-            .firestore()
-            .collection('timeline');
-
-        var atividade = null;
-        var id = null;
-
-        admin.firestore()
-            .collection('atividades')
-            .doc(politicoId)
-            .collection('atividadesPolitico')
-            .doc(documentId).get().then(function (doc) {
-                if (doc.exists) {
-                    id = doc.id;
-                    atividade = doc.data();
-                    admin.firestore()
-                        .collection('usuarios_seguindo')
-                        .doc(politicoId)
-                        .collection('usuariosSeguindo')
-                        .get().then(function (querySnapshot) {
-                            querySnapshot.forEach(function (doc) {
-                                timelineRef.doc(doc.id).collection('atividadesTimeline').doc(id).create(atividade);
-                            });
-                        });
-                }
-            });
-    })
