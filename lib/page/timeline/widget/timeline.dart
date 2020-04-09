6eb1aecb58db3f1b1daa9705c405b3d9bb9d87a2
Rodrigo <rodrigobastosv@gmail.com>
2020-04-09 01:03:22 -0300

    agruping on timeline

--- lib/page/timeline/widget/timeline.dart
+++ lib/page/timeline/widget/timeline.dart
@@ -1,31 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/flutter_widgets.dart';
 
 import '../../../model/models.dart';
 import '../../../widget/tile/despesa_tile_connected.dart';
 import '../../../widget/tile/proposta_tile_connected.dart';
 
-class Timeline extends StatelessWidget {
+class Timeline extends StatefulWidget {
   Timeline({this.activities});
 
   final List<dynamic> activities;
 
   @override
+  _TimelineState createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  String currentHoverDate;
+
+  @override
+  void initState() {
+    currentHoverDate = '';
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.only(top: 8),
-      itemBuilder: (_, i) {
-        if (activities[i] is DespesaModel) {
-          return DespesaTileConnected(activities[i] as DespesaModel);
-        } else {
-          return PropostaTileConnected(activities[i] as PropostaModel);
-        }
-      },
-      separatorBuilder: (_, i) => const Divider(
-        height: 16,
-        indent: 8,
-        endIndent: 8,
-      ),
-      itemCount: activities.length,
+    return Stack(
+      children: <Widget>[
+        ListView.separated(
+          padding: const EdgeInsets.only(top: 8),
+          itemBuilder: (_, i) {
+            if (widget.activities[i] is DespesaModel) {
+              return VisibilityDetector(
+                key: UniqueKey(),
+                onVisibilityChanged: (visibility) {
+                  if (visibility.visibleFraction == 1.0) {
+                    final despesa = widget.activities[i] as DespesaModel;
+                    setState(() => currentHoverDate = despesa.dataDocumento);
+                  }
+                },
+                child:
+                    DespesaTileConnected(widget.activities[i] as DespesaModel),
+              );
+            } else {
+              return VisibilityDetector(
+                key: UniqueKey(),
+                onVisibilityChanged: (visibility) {
+                  if (visibility.visibleFraction == 1.0) {
+                    final proposta = widget.activities[i] as PropostaModel;
+                    setState(() => currentHoverDate = proposta.dataDocumento);
+                  }
+                },
+                child: PropostaTileConnected(
+                    widget.activities[i] as PropostaModel),
+              );
+            }
+          },
+          separatorBuilder: (_, i) => const Divider(
+            height: 16,
+            indent: 8,
+            endIndent: 8,
+          ),
+          itemCount: widget.activities.length,
+        ),
+        Positioned(
+          left: MediaQuery.of(context).size.width / 2 - 40,
+          child: Container(
+            child: Text(currentHoverDate),
+            height: 40,
+            width: 120,
+            color: Colors.blueAccent,
+          ),
+        )
+      ],
     );
   }
 }
