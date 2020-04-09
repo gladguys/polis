30607ad047fc784d26af98c800a14b524d0387f9
Alexandre Lima <alexlima.crvg@gmail.com>
2020-04-09 18:12:55 -0300

    adicionando botão para atualizações na timeline

--- lib/page/timeline/widget/timeline.dart
+++ lib/page/timeline/widget/timeline.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../../i18n/i18n.dart';
 import '../../../model/models.dart';
+import '../../../widget/text_rich.dart';
 import '../../../widget/tile/despesa_tile_connected.dart';
 import '../../../widget/tile/proposta_tile_connected.dart';
 
@@ -11,6 +13,19 @@ class Timeline extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: <Widget>[
+        _buildList(),
+        Positioned(
+          top: 8,
+          child: _buildUpdateButton(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildList() {
     return ListView.separated(
       padding: const EdgeInsets.only(top: 8),
       itemBuilder: (_, i) {
@@ -26,6 +41,29 @@ class Timeline extends StatelessWidget {
         endIndent: 8,
       ),
       itemCount: activities.length,
+    );
+  }
+
+  Widget _buildUpdateButton() {
+    return Container(
+      height: 26,
+      child: RaisedButton(
+        child: TextRich(
+          fontSize: 12,
+          children: <InlineSpan>[
+            TextSpan(
+              text: '10', // TODO: trazer qtd de atualizações
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text: ' $UPDATES',
+            ),
+          ],
+        ),
+        onPressed: () {},
+      ),
     );
   }
 }
