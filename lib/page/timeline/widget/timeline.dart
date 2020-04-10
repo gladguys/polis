8b9f02eee7117b270841dfb4ea673240ea739b02
Rodrigo <rodrigobastosv@gmail.com>
2020-04-09 22:33:13 -0300

    changes to timeline

--- lib/page/timeline/widget/timeline.dart
+++ lib/page/timeline/widget/timeline.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../bloc/blocs.dart';
 import '../../../model/models.dart';
 import '../../../widget/tile/despesa_tile_connected.dart';
 import '../../../widget/tile/proposta_tile_connected.dart';
 
-class Timeline extends StatelessWidget {
+class Timeline extends StatefulWidget {
   Timeline({this.activities});
 
   final List<dynamic> activities;
 
   @override
+  _TimelineState createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  ScrollController scrollController;
+
+  @override
+  void initState() {
+    scrollController = ScrollController();
+    scrollController.addListener(_onScrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(_onScrollListener);
+    super.dispose();
+  }
+
+  void _onScrollListener() {
+    if (scrollController.offset >= scrollController.position.maxScrollExtent &&
+        !scrollController.position.outOfRange) {
+      final userId = context.bloc<UserBloc>().user.userId;
+      context.bloc<TimelineBloc>().add(FetchMorePosts(userId));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView.separated(
+      key: UniqueKey(),
+      controller: scrollController,
       padding: const EdgeInsets.only(top: 8),
       itemBuilder: (_, i) {
-        if (activities[i] is DespesaModel) {
-          return DespesaTileConnected(activities[i] as DespesaModel);
+        if (widget.activities[i] is DespesaModel) {
+          return DespesaTileConnected(widget.activities[i] as DespesaModel);
         } else {
-          return PropostaTileConnected(activities[i] as PropostaModel);
+          return PropostaTileConnected(widget.activities[i] as PropostaModel);
         }
       },
       separatorBuilder: (_, i) => const Divider(
@@ -25,7 +57,7 @@ class Timeline extends StatelessWidget {
         indent: 8,
         endIndent: 8,
       ),
-      itemCount: activities.length,
+      itemCount: widget.activities.length,
     );
   }
 }
