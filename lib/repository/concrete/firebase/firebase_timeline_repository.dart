47ad0b9681d7f4fcbe2d902783d9ce35dad5f32e
Rodrigo <rodrigobastosv@gmail.com>
2020-04-09 22:32:44 -0300

    repositories

--- lib/repository/concrete/firebase/firebase_timeline_repository.dart
+++ lib/repository/concrete/firebase/firebase_timeline_repository.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
 
 import '../../../core/exception/exceptions.dart';
 import '../../../model/models.dart';
@@ -40,5 +41,65 @@ class FirebaseTimelineRepository implements TimelineRepository {
     } on Exception {
       throw ComunicationException();
     }
+  }
+
+  @override
+  Future<Tuple2<List<dynamic>, DocumentSnapshot>> getTimelineFirstPosts(
+      String userId, int count) async {
+    try {
+      final query = await timelineRef
+          .document(userId)
+          .collection(ATIVIDADES_TIMELINE_SUBCOLLECTION)
+          .orderBy(DATA_DOCUMENTO_FIELD, descending: true)
+          .limit(count);
+
+      final querySnapshot = await query.getDocuments();
+      final activities = getActivitiesFromSnapshot(querySnapshot);
+
+      return Tuple2<List<dynamic>, DocumentSnapshot>(
+        activities,
+        querySnapshot.documents.last,
+      );
+    } on Exception {
+      throw ComunicationException();
+    }
+  }
+
+  @override
+  Future<Tuple2<List, DocumentSnapshot>> getMorePosts(
+      String userId, int count, DocumentSnapshot lastDocument) async {
+    try {
+      final query = await timelineRef
+          .document(userId)
+          .collection(ATIVIDADES_TIMELINE_SUBCOLLECTION)
+          .orderBy(DATA_DOCUMENTO_FIELD, descending: true)
+          .startAfterDocument(lastDocument)
+          .limit(count);
+
+      final querySnapshot = await query.getDocuments();
+      final activities = getActivitiesFromSnapshot(querySnapshot);
+
+      return Tuple2<List<dynamic>, DocumentSnapshot>(
+        activities,
+        querySnapshot.documents.last,
+      );
+    } on Exception {
+      throw ComunicationException();
+    }
+  }
+
+  List<dynamic> getActivitiesFromSnapshot(QuerySnapshot querySnapshot) {
+    final activities = <dynamic>[];
+
+    final documents = querySnapshot.documents;
+    for (var document in documents) {
+      if (isDocumentDespesa(document.data)) {
+        final despesaModel = DespesaModel.fromJson(document.data);
+        activities.add(despesaModel.copyWith(id: document.documentID));
+      } else {
+        activities.add(PropostaModel.fromJson(document.data));
+      }
+    }
+    return activities;
   }
 }
