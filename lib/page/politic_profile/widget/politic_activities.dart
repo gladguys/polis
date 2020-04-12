6240a68932ccbbef5a9eccf0565ecf534981f2ee
Alexandre Lima <alexlima.crvg@gmail.com>
2020-04-12 20:06:19 -0300

    adicionando slidepanel a tela do político

--- lib/page/politic_profile/widget/politic_activities.dart
+++ lib/page/politic_profile/widget/politic_activities.dart
@@ -4,7 +4,6 @@ import '../../../i18n/i18n.dart';
 import '../../../model/despesa_model.dart';
 import '../../../model/models.dart';
 import '../../../widget/not_found.dart';
-import '../../../widget/text_title.dart';
 import '../../../widget/tile/despesa_tile_connected.dart';
 import '../../../widget/tile/proposta_tile_connected.dart';
 
@@ -15,31 +14,24 @@ class PoliticActivities extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextTitle(LAST_ACTIVITIES, fontSize: 15),
-        const SizedBox(height: 8),
-        Expanded(
-          child: _getActivities(lastActivities),
-        ),
-      ],
-    );
-  }
-
-  Widget _getActivities(List<dynamic> lastActivities) {
     if (lastActivities.isNotEmpty) {
-      return ListView.builder(
-        shrinkWrap: true,
-        itemBuilder: (_, i) {
-          final activity = lastActivities[i];
-          if (activity is DespesaModel) {
-            return DespesaTileConnected(activity, clickableImage: false);
-          } else {
-            final proposicao = activity as PropostaModel;
-            return PropostaTileConnected(proposicao, clickableImage: false);
-          }
-        },
-        itemCount: lastActivities.length,
+      return Container(
+        height: (MediaQuery.of(context).size.height * 0.97) - 50 - 72,
+        child: ListView.separated(
+          shrinkWrap: true,
+          padding: const EdgeInsets.only(bottom: 24),
+          separatorBuilder: (_, i) => const Divider(indent: 8, endIndent: 8),
+          itemBuilder: (_, i) {
+            final activity = lastActivities[i];
+            if (activity is DespesaModel) {
+              return DespesaTileConnected(activity, clickableImage: false);
+            } else {
+              final proposicao = activity as PropostaModel;
+              return PropostaTileConnected(proposicao, clickableImage: false);
+            }
+          },
+          itemCount: lastActivities.length,
+        ),
       );
     } else {
       return NotFound(msg: NO_ACTIVITY_FOR_POLITIC);
