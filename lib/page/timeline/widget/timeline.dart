05bbaef5867daab125d1c3fdf2e2c9929b040a90
Alexandre Lima <alexlima.crvg@gmail.com>
2020-04-15 20:49:57 -0300

    trocando groupedlist por listbuilder

--- lib/page/timeline/widget/timeline.dart
+++ lib/page/timeline/widget/timeline.dart
@@ -2,16 +2,13 @@ import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:grouped_list/grouped_list.dart';
 
 import '../../../bloc/blocs.dart';
-import '../../../extension/formatters.dart';
 import '../../../i18n/i18n.dart';
 import '../../../model/models.dart';
 import '../../../widget/text_rich.dart';
 import '../../../widget/tile/despesa_tile_connected.dart';
 import '../../../widget/tile/proposta_tile_connected.dart';
-import '../../theme/main_theme.dart';
 
 class Timeline extends StatefulWidget {
   Timeline({@required this.activities, @required this.updatesCount})
@@ -42,6 +39,12 @@ class _TimelineState extends State<Timeline> {
     super.initState();
   }
 
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   void _onScrollListener() {
     if (currentPosition >= maxScrollPosition && isPositionInRange) {
       timelineBloc.add(FetchMorePosts(userId));
@@ -65,60 +68,23 @@ class _TimelineState extends State<Timeline> {
   }
 
   Widget _buildList() {
-    return GroupedListView(
+    return ListView.separated(
       controller: scrollController,
-      useStickyGroupSeparators: true,
-      elements: widget.activities,
-      order: GroupedListOrder.DESC,
-      groupBy: (element) => element.dataDocumento,
-      groupSeparatorBuilder: (value) {
-        return Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Positioned(
-              left: 0,
-              width: MediaQuery.of(context).size.width,
-              child: Divider(
-                thickness: 1,
-                height: 1,
-                color: theme.primaryColorLight,
-              ),
-            ),
-            Center(
-              child: Container(
-                width: 100,
-                height: 20,
-                margin: const EdgeInsets.symmetric(vertical: 4),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: theme.primaryColorLight,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Text(
-                  value.toString().formatDate(),
-                  style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        );
-      },
-      indexedItemBuilder: (_, element, index) {
+      padding: const EdgeInsets.only(top: 8, bottom: 16),
+      itemCount: widget.activities.length,
+      itemBuilder: (_, i) {
         return Column(
           children: <Widget>[
-            if (element is DespesaModel)
-              DespesaTileConnected(element)
+            if (widget.activities[i] is DespesaModel)
+              DespesaTileConnected(widget.activities[i])
             else
-              PropostaTileConnected(element as PropostaModel),
-            if ((index == 2) || (index > 2 && index % 5 == 0))
+              PropostaTileConnected(widget.activities[i] as PropostaModel),
+            if ((i == 2) || (i > 2 && i % 5 == 0))
               _buildAdmobBanner(),
           ],
         );
       },
-      separator: const Divider(
+      separatorBuilder: (_, i) => const Divider(
         height: 16,
         indent: 8,
         endIndent: 8,
--- lib/page/timeline/widget/timeline_skeleton.dart
+++ lib/page/timeline/widget/timeline_skeleton.dart
@@ -6,56 +6,48 @@ import '../../../widget/skeleton.dart';
 class TimelineSkeleton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        const SizedBox(height: 4),
-        const Skeleton(width: 100, height: 20),
-        Expanded(
-          child: ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            padding: const EdgeInsets.only(top: 8),
-            separatorBuilder: (context, index) => const SizedBox(height: 16),
-            itemCount: 10,
-            itemBuilder: (context, i) {
-              return Row(
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.only(top: 8),
+      separatorBuilder: (context, index) => const SizedBox(height: 16),
+      itemCount: 10,
+      itemBuilder: (context, i) {
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(width: 8),
+            const Skeleton(width: 48, height: 48),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  const SizedBox(width: 8),
-                  const Skeleton(width: 48, height: 48),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const SizedBox(height: 4),
-                        const Skeleton(width: 180, height: 14),
-                        const SizedBox(height: 4),
-                        const Skeleton(width: 240, height: 24),
-                        const SizedBox(height: 4),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            const Skeleton(width: 20, height: 20),
-                            const SizedBox(width: 16),
-                            const Skeleton(width: 20, height: 20),
-                            const SizedBox(width: 16),
-                            const Skeleton(width: 20, height: 20),
-                            const Spacer(flex: 1),
-                            const Skeleton(width: 20, height: 20),
-                            const SizedBox(width: 8),
-                            const Skeleton(width: 20, height: 20),
-                            const SizedBox(width: 8),
-                          ],
-                        ),
-                      ],
-                    ),
+                  const SizedBox(height: 4),
+                  const Skeleton(width: 180, height: 14),
+                  const SizedBox(height: 4),
+                  const Skeleton(width: 240, height: 24),
+                  const SizedBox(height: 4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      const Skeleton(width: 20, height: 20),
+                      const SizedBox(width: 16),
+                      const Skeleton(width: 20, height: 20),
+                      const SizedBox(width: 16),
+                      const Skeleton(width: 20, height: 20),
+                      const Spacer(flex: 1),
+                      const Skeleton(width: 20, height: 20),
+                      const SizedBox(width: 8),
+                      const Skeleton(width: 20, height: 20),
+                      const SizedBox(width: 8),
+                    ],
                   ),
                 ],
-              );
-            },
-          ),
-        ),
-      ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
