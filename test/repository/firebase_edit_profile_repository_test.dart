8fd197c360303c64e002b4ed23eb540a657f9420
rodrigobastosv <rodrigobastosv@gmail.com>
2020-04-17 07:57:48 -0300

    finishing tests

--- test/repository/firebase_edit_profile_repository_test.dart
+++ test/repository/firebase_edit_profile_repository_test.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:polis/core/exception/exceptions.dart';
 import 'package:polis/model/models.dart';
 import 'package:polis/repository/concrete/firebase/collection.dart';
 import 'package:polis/repository/concrete/repositories.dart';
@@ -15,15 +16,11 @@ void main() {
   MockFirebaseStorage mockFirebaseStorage;
   MockCollectionReference mockUsersCollectionReference;
   MockDocumentReference mockUserDocumentReference;
-  MockQuerySnapshot mockQuerySnapshot;
   MockStorageReference mockStorageReference;
   MockStorageReference mockUsersStorageReference;
   MockStorageReference imageRef;
   MockStorageTaskSnapshot mockStorageTaskSnapshot;
   MockStorageUploadTask mockStorageUploadTask;
-  MockDocumentSnapshot mockDespesaDocumentSnapshot;
-  MockDocumentSnapshot mockPropostaDocumentSnapshot;
-  MockQuery mockQuery;
 
   group('FirebasePostRepository tests', () {
     setUp(() {
@@ -42,14 +39,29 @@ void main() {
       );
       mockUsersCollectionReference = MockCollectionReference();
       mockUserDocumentReference = MockDocumentReference();
-      mockQuerySnapshot = MockQuerySnapshot();
-      mockDespesaDocumentSnapshot = MockDocumentSnapshot();
-      mockPropostaDocumentSnapshot = MockDocumentSnapshot();
-      mockQuery = MockQuery();
     });
 
     test('test asserts', () {
-      expect(() => FirebaseFavoritePostsRepository(firestore: null),
+      expect(
+          () => FirebaseEditProfileRepository(
+                firestore: null,
+                firebaseAuth: mockFirebaseAuth,
+                firebaseStorage: mockFirebaseStorage,
+              ),
+          throwsAssertionError);
+      expect(
+          () => FirebaseEditProfileRepository(
+                firestore: mockFirestore,
+                firebaseAuth: null,
+                firebaseStorage: mockFirebaseStorage,
+              ),
+          throwsAssertionError);
+      expect(
+          () => FirebaseEditProfileRepository(
+                firestore: mockFirestore,
+                firebaseAuth: mockFirebaseAuth,
+                firebaseStorage: null,
+              ),
           throwsAssertionError);
     });
 
@@ -59,6 +71,10 @@ void main() {
         when(mockStorageReference.child(USERS_COLLECTION))
             .thenReturn(mockUsersStorageReference);
         when(mockUsersStorageReference.child('1')).thenReturn(imageRef);
+        when(mockFirestore.collection(USERS_COLLECTION))
+            .thenReturn(mockUsersCollectionReference);
+        when(mockUsersCollectionReference.document('1'))
+            .thenReturn(mockUserDocumentReference);
 
         when(imageRef.putFile(any)).thenReturn(mockStorageUploadTask);
         when(mockStorageUploadTask.onComplete)
@@ -74,6 +90,75 @@ void main() {
             'assets/images/google.png',
           ),
         );
+      });
+
+      test('should throw exception when upload image fail', () async {
+        when(mockFirebaseStorage.ref()).thenReturn(mockStorageReference);
+        when(mockStorageReference.child(USERS_COLLECTION))
+            .thenReturn(mockUsersStorageReference);
+        when(mockUsersStorageReference.child('1')).thenReturn(imageRef);
+
+        when(imageRef.putFile(any)).thenThrow(Exception());
+
+        firebaseEditProfileRepository
+            .updateUserInfo(
+              currentUser: UserModel(userId: '1'),
+              name: 'name',
+              email: 'email',
+              pickedPhoto: File(
+                'assets/images/google.png',
+              ),
+            )
+            .catchError((e) => expect(e, isA<UploadFileException>()));
+      });
+
+      test('should not upload image when file is null', () async {
+        when(mockFirebaseStorage.ref()).thenReturn(mockStorageReference);
+        when(mockStorageReference.child(USERS_COLLECTION))
+            .thenReturn(mockUsersStorageReference);
+        when(mockUsersStorageReference.child('1')).thenReturn(imageRef);
+        when(mockFirestore.collection(USERS_COLLECTION))
+            .thenReturn(mockUsersCollectionReference);
+        when(mockUsersCollectionReference.document('1'))
+            .thenReturn(mockUserDocumentReference);
+
+        when(imageRef.putFile(any)).thenReturn(mockStorageUploadTask);
+        when(mockStorageUploadTask.onComplete)
+            .thenAnswer((_) => Future.value(mockStorageTaskSnapshot));
+        when(imageRef.getDownloadURL())
+            .thenAnswer((_) => Future.value('my-photo-url'));
+
+        await firebaseEditProfileRepository.updateUserInfo(
+          currentUser: UserModel(userId: '1'),
+          name: 'name',
+          email: 'email',
+          pickedPhoto: null,
+        );
+      });
+
+      test('should throw exception when updating user', () async {
+        when(mockFirebaseStorage.ref()).thenReturn(mockStorageReference);
+        when(mockStorageReference.child(USERS_COLLECTION))
+            .thenReturn(mockUsersStorageReference);
+        when(mockUsersStorageReference.child('1')).thenReturn(imageRef);
+        when(mockFirestore.collection(USERS_COLLECTION))
+            .thenReturn(mockUsersCollectionReference);
+        when(mockUsersCollectionReference.document('1')).thenThrow(Exception());
+
+        when(imageRef.putFile(any)).thenReturn(mockStorageUploadTask);
+        when(mockStorageUploadTask.onComplete)
+            .thenAnswer((_) => Future.value(mockStorageTaskSnapshot));
+        when(imageRef.getDownloadURL())
+            .thenAnswer((_) => Future.value('my-photo-url'));
+
+        firebaseEditProfileRepository
+            .updateUserInfo(
+              currentUser: UserModel(userId: '1'),
+              name: 'name',
+              email: 'email',
+              pickedPhoto: null,
+            )
+            .catchError((e) => expect(e, isA<ComunicationException>()));
       });
     });
   });
