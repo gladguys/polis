6f99ec39c9d83d77c24afc302a548d73c9824486
Alexandre Lima <alexlima.crvg@gmail.com>
2020-04-22 22:16:10 -0300

    alterando posição da situação da proposta e verificando quando é emenda

--- lib/widget/post/post_proposta.dart
+++ lib/widget/post/post_proposta.dart
@@ -81,15 +81,20 @@ class PostProposta extends StatelessWidget {
         spacing: 16,
         runSpacing: 8,
         children: <Widget>[
-          TextRich(
-            children: [
-              TextSpan(
-                text: '${proposta.descricaoTipo}: ',
-                style: const TextStyle(fontWeight: FontWeight.w500),
-              ),
-              TextSpan(text: '${proposta.ementa ?? NOT_INFORMED_FEMALE}'),
-            ],
-          ),
+          if (proposta.descricaoTipo == 'Emenda de Plenário')
+            Row(children: [
+              Text('${proposta.descricaoTipo}'),
+            ])
+          else
+            TextRich(
+              children: [
+                TextSpan(
+                  text: '${proposta.descricaoTipo}: ',
+                  style: const TextStyle(fontWeight: FontWeight.w500),
+                ),
+                TextSpan(text: '${proposta.ementa ?? NOT_INFORMED_FEMALE}'),
+              ],
+            ),
           LabelValue(
             label: DETAILED_MENU,
             value: proposta.ementaDetalhada,
