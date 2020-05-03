840489da9d6a4f2f9e8d66c9018aae9f49bf013b
Rodrigo <rodrigobastosv@gmail.com>
2020-05-03 14:29:33 -0300

    more adjusts

--- functions/lib/onDeleteFollower.js
+++ functions/lib/onDeleteFollower.js
@@ -88,7 +88,7 @@ exports.onDeleteFollower = functions.firestore
                 'idPolitico': politicoId,
                 'mensagem': 'Deixei de seguir o político ' + p.data().nomeEleitoral + ' no dia ' + today,
                 'urlFotoPolitico': p.data().urlFoto,
-                'sexoPolitico': p.sexo
+                'sexoPolitico': p.data().sexo
             };
 
             admin.firestore().collection('acoes').doc(followerId).collection('acoesUsuario').add(acao);
