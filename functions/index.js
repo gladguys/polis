95ab6a911206f1e645627d77e32ab48ff2d3b991
Rodrigo <rodrigobastosv@gmail.com>
2020-05-03 07:40:46 -0300

    adding cloud function to update followers profile photo when changed

--- functions/index.js
+++ functions/index.js
@@ -264,3 +264,37 @@ exports.onCreateActivity = functions.firestore
                 }
             });
     })
+
+/**
+ * Update photoUrl field on the collection usuarios_seguindo of each politic that the user follows.
+ * This function only do something when the user in fact changes his photo
+ * @type {CloudFunction<DocumentSnapshot>}
+ */
+exports.onUpdateUser = functions.firestore
+    .document('/users/{userId}')
+    .onCreate(async (change, context) => {
+        const userId = context.params.userId;
+        const previousUser = change.before.data();
+        const newUser = change.after.data();
+
+        if (newUser.photoURL !== previousUser.photoURL) {
+            const newPhotoUrl = newUser.photoURL;
+
+            const politicosSeguidosUsuario = await admin
+                .firestore()
+                .collection('politicos_seguidos')
+                .doc(userId)
+                .collection('politicosSeguidos')
+                .get();
+            
+            politicosSeguidosUsuario.forEach(async (politico) => {
+                await admin
+                    .firestore()
+                    .collection('usuarios_seguindo')
+                    .doc(politico.id)
+                    .collection('usuariosSeguindo')
+                    .doc(userId)
+                    .update({ "photoUrl": newPhotoUrl });
+            });
+        }
+    })
