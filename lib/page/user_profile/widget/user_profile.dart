32bd505f2548f05882ed65c35cd8a1c3f123180b
Alexandre Lima <alexlima.crvg@gmail.com>
2020-05-04 23:26:19 -0300

    aplicando novo componente para slidepanel no perfil de usuário

--- lib/page/user_profile/widget/user_profile.dart
+++ lib/page/user_profile/widget/user_profile.dart
@@ -1,8 +1,8 @@
+import 'package:expandable_bottom_sheet/expandable_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:simple_router/simple_router.dart';
-import 'package:sliding_panel/sliding_panel.dart';
 
 import '../../../bloc/blocs.dart';
 import '../../../bloc/user/user_bloc.dart';
@@ -28,70 +28,56 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
-  PanelController _panelController;
-
-  @override
-  void initState() {
-    _panelController = PanelController();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return SlidingPanel(
-      panelController: _panelController,
-      parallaxSlideAmount: 0,
-      isTwoStatePanel: true,
-      isDraggable: true,
-      decoration: PanelDecoration(
-        backgroundColor: theme.scaffoldBackgroundColor,
-        boxShadows: [
-          BoxShadow(color: Colors.black26, blurRadius: 3),
+    return ExpandableBottomSheet(
+      persistentContentHeight: MediaQuery.of(context).size.height - 460,
+      persistentHeader: _buildHeader(),
+      expandableContent: _buildPanel(context),
+      background: _buildBody(context),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        color: theme.scaffoldBackgroundColor,
+        boxShadow: [
+          const BoxShadow(
+            color: Colors.black12,
+            blurRadius: 1,
+            offset: Offset(0, -3),
+          ),
         ],
         borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(24),
           topRight: Radius.circular(24),
         ),
       ),
-      size: const PanelSize(
-        closedHeight: 0.45,
-        expandedHeight: 0.97,
-      ),
-      content: PanelContent(
-        headerWidget: PanelHeaderWidget(
-          headerContent: _buildHeader(),
-          options: const PanelHeaderOptions(centerTitle: true),
-          decoration: const PanelDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(24),
-              topRight: Radius.circular(24),
+      child: Column(
+        children: <Widget>[
+          const SizedBox(height: 6),
+          Container(
+            width: 40,
+            height: 4,
+            decoration: BoxDecoration(
+              color: theme.accentColor.withOpacity(.25),
+              borderRadius: BorderRadius.circular(4),
             ),
           ),
-        ),
-        panelContent: <Widget>[
-          _buildPanel(context),
+          const SizedBox(height: 8),
+          TextTitle(MY_ACTIVITIES, fontSize: 15),
+          const SizedBox(height: 4),
         ],
-        bodyContent: _buildBody(context),
       ),
     );
   }
 
-  Widget _buildHeader() {
-    return Column(
-      children: <Widget>[
-        const SizedBox(height: 6),
-        Container(
-          width: 40,
-          height: 4,
-          decoration: BoxDecoration(
-            color: theme.accentColor.withOpacity(.25),
-            borderRadius: BorderRadius.circular(4),
-          ),
-        ),
-        const SizedBox(height: 8),
-        TextTitle(MY_ACTIVITIES, fontSize: 15),
-        const SizedBox(height: 4),
-      ],
+  Widget _buildPanel(BuildContext context) {
+    return Container(
+      color: theme.scaffoldBackgroundColor,
+      child: UserActions(actions: widget.userActions),
     );
   }
 
@@ -137,9 +123,5 @@ class _UserProfileState extends State<UserProfile> {
         const SizedBox(width: 8),
       ],
     );
-  }
-
-  Widget _buildPanel(BuildContext context) {
-    return UserActions(actions: widget.userActions);
   }
 }
