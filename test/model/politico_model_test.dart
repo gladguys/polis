e2dcec17f8d63380ea0dd7ff931400a0fa71af7c
rodrigobastosv <rodrigobastosv@gmail.com>
2020-05-15 00:05:10 -0300

    adding test

--- test/model/politico_model_test.dart
+++ test/model/politico_model_test.dart
@@ -61,7 +61,7 @@ void main() {
     test('toString()', () {
       final modelToString = PoliticoModel(id: '1').toString();
       final result =
-          '''PoliticoModel{id: 1, nomeCivil: null, siglaPartido: null, siglaUf: null, urlFoto: null, email: null, nomeEleitoral: null, status: null, condicaoEleitoral: null, cpf: null, sexo: null, quantidadeSeguidores: null, dataNascimento: null, escolaridade: null, totalDespesas: null, totalProposicoes: null}''';
+          '''PoliticoModel{id: 1, nomeCivil: null, siglaPartido: null, siglaUf: null, urlFoto: null, email: null, nomeEleitoral: null, status: null, condicaoEleitoral: null, cpf: null, sexo: null, quantidadeSeguidores: null, dataNascimento: null, escolaridade: null, totalDespesas: null, totalProposicoes: null, rankingPosDespesa: null}''';
       expect(modelToString == result, true);
     });
 
