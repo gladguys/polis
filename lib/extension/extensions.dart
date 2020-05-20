567c97b9472e838284af27161d1de45ada50aa21
rodrigobastosv <rodrigobastosv@gmail.com>
2020-05-20 09:44:01 -0300

    adjusting timeline ad

--- lib/extension/extensions.dart
+++ lib/extension/extensions.dart
@@ -1,3 +1,4 @@
 export 'formatter_extensions.dart';
 export 'media_query_extensions.dart';
+export 'theme_extensions.dart';
 export 'time_extensions.dart';
---
+++ lib/widget/ad/timeline_ad_banner.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_admob/flutter_native_admob.dart';
+import 'package:flutter_native_admob/native_admob_controller.dart';
+
+import '../../extension/extensions.dart';
+
+class TimelineAdBanner extends StatelessWidget {
+  TimelineAdBanner(this.nativeAdmobController);
+
+  final NativeAdmobController nativeAdmobController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Container(
+            height: 100,
+            color: context.baseBackgroundColor,
+            child: NativeAdmob(
+              loading: Container(),
+              adUnitID: 'ca-app-pub-5806526425473649/3495270844',
+              controller: nativeAdmobController,
+              type: NativeAdmobType.banner,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
