c2154398b0b241d0ad805bfd1ed36e1c645e17bf
rodrigobastosv <rodrigobastosv@gmail.com>
2020-05-20 14:09:21 -0300

    more tests

--- test/widget/my_app_test.dart
+++ test/widget/my_app_test.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:polis/bloc/blocs.dart';
 import 'package:polis/core/service/locator.dart';
+import 'package:polis/model/models.dart';
 import 'package:polis/page/pages.dart';
 import 'package:polis/widget/my_app.dart';
 import 'package:polis/widget/my_app_injections.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../mock.dart';
 
 void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -17,14 +21,49 @@ void main() async {
   channel.setMockMethodCallHandler((methodCall) async => true);
   initLocator(await SharedPreferences.getInstance());
 
+  test('assert', () {
+    expect(
+        () => MyApp(
+              sharedPreferencesService: null,
+            ),
+        throwsAssertionError);
+  });
+
   testWidgets('MyApp tests', (tester) async {
+    final mockSharedPreferencesService = MockSharedPreferencesService();
+    when(mockSharedPreferencesService.getUser()).thenReturn(
+      UserModel(
+        userId: '1',
+      ),
+    );
+    when(mockSharedPreferencesService.isUserLogged()).thenReturn(false);
     await tester.pumpWidget(
       MyAppInjections(
-        child: MyApp(),
+        child: MyApp(
+          sharedPreferencesService: mockSharedPreferencesService,
+        ),
       ),
     );
     final context = tester.element(find.byType(InitialPage));
     expect(find.byType(MyApp), findsOneWidget);
     expect(context.bloc<UserBloc>(), isNotNull);
+  });
+
+  testWidgets('Should go to timeline when user is logged', (tester) async {
+    final mockSharedPreferencesService = MockSharedPreferencesService();
+    when(mockSharedPreferencesService.getUser()).thenReturn(
+      UserModel(
+        userId: '1',
+      ),
+    );
+    when(mockSharedPreferencesService.isUserLogged()).thenReturn(true);
+    await tester.pumpWidget(
+      MyAppInjections(
+        child: MyApp(
+          sharedPreferencesService: mockSharedPreferencesService,
+        ),
+      ),
+    );
+    expect(find.byType(TimelinePage), findsOneWidget);
   });
 }
