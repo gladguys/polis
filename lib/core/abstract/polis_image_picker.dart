f7d8a2d666fa2fa58948697949e02c4091e4dad5
rodrigobastosv <rodrigobastosv@gmail.com>
2020-06-04 23:05:08 -0300

    no time to do this now

--- lib/core/abstract/polis_image_picker.dart
+++ lib/core/abstract/polis_image_picker.dart
@@ -4,6 +4,8 @@ import 'package:image_picker/image_picker.dart';
 
 class PolisImagePicker {
   Future<File> getImage() async {
+    // TODO(rodrigo): Refactor this use
+    // ignore: deprecated_member_use
     return await ImagePicker.pickImage(source: ImageSource.camera);
   }
 }
