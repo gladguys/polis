95906924437c46b42ca5ee5d832e13c034a53b33
rodrigobastosv <rodrigobastosv@gmail.com>
2020-06-29 16:02:20 -0300

    adding the model

---
+++ lib/core/domain/model/comment_model.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+class CommentModel extends Equatable {
+  CommentModel({
+    this.id,
+    this.userId,
+    this.texto,
+    this.data,
+    this.comentariosFilhos,
+  });
+
+  final String id;
+  final String userId;
+  final String texto;
+  final String data;
+  final List<CommentModel> comentariosFilhos;
+
+  CommentModel copyWith({
+    String id,
+    String userId,
+    String texto,
+    String data,
+    List<CommentModel> comentariosFilhos,
+  }) {
+    return CommentModel(
+      id: id ?? this.id,
+      userId: userId ?? this.userId,
+      texto: texto ?? this.texto,
+      data: data ?? this.data,
+      comentariosFilhos: comentariosFilhos ?? this.comentariosFilhos,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''CommentModel{id: $id, userId: $userId, texto: $texto, data: $data, comentariosFilhos: $comentariosFilhos}''';
+  }
+
+  @override
+  List<Object> get props => [id, texto];
+}
