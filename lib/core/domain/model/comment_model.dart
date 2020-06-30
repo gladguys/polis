bc00407f66dc76196f9e06a044c1c91edef8131e
rodrigobastosv <rodrigobastosv@gmail.com>
2020-06-29 21:30:54 -0300

    add more fields

--- lib/core/domain/model/comment_model.dart
+++ lib/core/domain/model/comment_model.dart
@@ -4,6 +4,8 @@ class CommentModel extends Equatable {
   CommentModel({
     this.id,
     this.userId,
+    this.username,
+    this.userPhoto,
     this.texto,
     this.data,
     this.respostas,
@@ -11,6 +13,8 @@ class CommentModel extends Equatable {
 
   final String id;
   final String userId;
+  final String username;
+  final String userPhoto;
   final String texto;
   final String data;
   final List<CommentModel> respostas;
@@ -18,6 +22,8 @@ class CommentModel extends Equatable {
   CommentModel copyWith({
     String id,
     String userId,
+    String username,
+    String userPhoto,
     String texto,
     String data,
     List<CommentModel> respostas,
@@ -25,6 +31,8 @@ class CommentModel extends Equatable {
     return CommentModel(
       id: id ?? this.id,
       userId: userId ?? this.userId,
+      username: username ?? this.username,
+      userPhoto: userPhoto ?? this.userPhoto,
       texto: texto ?? this.texto,
       data: data ?? this.data,
       respostas: respostas ?? this.respostas,
@@ -33,7 +41,7 @@ class CommentModel extends Equatable {
 
   @override
   String toString() {
-    return '''CommentModel{id: $id, userId: $userId, texto: $texto, data: $data, respostas: $respostas}''';
+    return '''CommentModel{id: $id, userId: $userId, username: $username, userPhoto: $userPhoto, texto: $texto, data: $data, respostas: $respostas}''';
   }
 
   @override
