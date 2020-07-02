48a6180b939549c850f69c1411c9d6c34a0557b6
rodrigobastosv <rodrigobastosv@gmail.com>
2020-07-01 21:20:44 -0300

    saving actions on bloc

--- lib/bloc/post/post_bloc.dart
+++ lib/bloc/post/post_bloc.dart
@@ -5,10 +5,12 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+import '../../core/domain/enum/acao_type.dart';
 import '../../core/domain/model/models.dart';
 import '../../core/repository/abstract/repositories.dart';
 import '../../core/repository/concrete/firebase/firebase.dart';
 import '../../core/service/services.dart';
+import '../../core/utils/general_utils.dart';
 import '../blocs.dart';
 
 part 'post_event.dart';
@@ -18,18 +20,21 @@ class PostBloc extends Bloc<PostEvent, PostState> {
   PostBloc({
     @required this.post,
     @required this.postRepository,
+    @required this.actionRepository,
     @required this.shareService,
     @required this.userBloc,
     this.timelineBloc,
   }) {
     assert(post != null);
     assert(postRepository != null);
+    assert(actionRepository != null);
     assert(shareService != null);
     assert(userBloc != null);
     isPostFavorite = post[FAVORITO_FIELD] ?? false;
   }
 
   final PostRepository postRepository;
+  final ActionRepository actionRepository;
   final ShareService shareService;
   final UserBloc userBloc;
   final TimelineBloc timelineBloc;
@@ -165,8 +170,9 @@ class PostBloc extends Bloc<PostEvent, PostState> {
   Stream<PostState> _mapLikePostToState(LikePost event) async* {
     try {
       final isUnliked = event.isUnliked ?? false;
+      final user = event.user;
       final likes = await postRepository.likePost(
-        userId: event.user.userId,
+        userId: user.userId,
         postId: event.postId,
         politicoId: event.politicoId,
         isUnliked: isUnliked,
@@ -177,11 +183,18 @@ class PostBloc extends Bloc<PostEvent, PostState> {
         QTD_NAO_CURTIDAS_FIELD:
             (post[QTD_NAO_CURTIDAS_FIELD] ?? 0) - (isUnliked ? 1 : 0),
       };
+
       userBloc.add(
         UpdateCurrentUser(
           userBloc.user
               .copyWith(userLikes: likes.item1, userUnlikes: likes.item2),
         ),
+      );
+
+      await _saveUserAction(
+        user: user,
+        post: post,
+        actionType: AcaoType.like,
       );
       yield PostLikedSuccess(
         postId: event.postId,
@@ -219,9 +232,10 @@ class PostBloc extends Bloc<PostEvent, PostState> {
 
   Stream<PostState> _mapUnlikePostToState(UnlikePost event) async* {
     try {
+      final user = event.user;
       final isLiked = event.isLiked ?? false;
       final likes = await postRepository.unlikePost(
-        userId: event.user.userId,
+        userId: user.userId,
         postId: event.postId,
         politicoId: event.politicoId,
         isLiked: isLiked,
@@ -239,6 +253,13 @@ class PostBloc extends Bloc<PostEvent, PostState> {
           ),
         ),
       );
+
+      await _saveUserAction(
+        user: user,
+        post: post,
+        actionType: AcaoType.unlike,
+      );
+
       yield PostUnlikedSuccess(
         postId: event.postId,
       );
@@ -270,6 +291,26 @@ class PostBloc extends Bloc<PostEvent, PostState> {
       );
     } on Exception {
       yield StoppedUnlikingPostFailed();
+    }
+  }
+
+  Future<void> _saveUserAction(
+      {UserModel user, dynamic post, AcaoType actionType}) async {
+    try {
+      await actionRepository.saveUserAction(
+        user: user,
+        acao: AcaoUsuarioModel(
+          idPost: getPostIdFromJson(post),
+          idPolitico: getPoliticoIdFromPostJson(post),
+          nomePolitico: post[NOME_POLITICO_FIELD],
+          urlFotoPolitico: post[FOTO_POLITICO_FIELD],
+          tipo: actionType,
+          tipoPost: getPostTypeFromJson(post),
+          data: DateTime.now(),
+        ),
+      );
+    } on Exception {
+      rethrow;
     }
   }
 }
