56aea620ab2ae7694742c6d803caf026632860fc
rodrigobastosv <rodrigobastosv@gmail.com>
2020-07-01 21:20:24 -0300

    big refactor to reuse more code

--- lib/core/repository/concrete/firebase/firebase_post_repository.dart
+++ lib/core/repository/concrete/firebase/firebase_post_repository.dart
@@ -4,6 +4,7 @@ import 'package:tuple/tuple.dart';
 
 import '../../../../core/domain/model/user_model.dart';
 import '../../../../core/exception/exceptions.dart';
+import '../../../utils/general_utils.dart';
 import '../../abstract/repositories.dart';
 import 'firebase.dart';
 
@@ -31,7 +32,7 @@ class FirebasePostRepository implements PostRepository {
       await postsFavoritosRef
           .document(user.userId)
           .collection(POSTS_FAVORITOS_USUARIO_SUBCOLLECTION)
-          .document(_getIdFromPost(post))
+          .document(getPostIdFromJson(post))
           .setData(postWithTimestamp);
     } on Exception {
       throw ComunicationException();
@@ -45,7 +46,7 @@ class FirebasePostRepository implements PostRepository {
       await postsFavoritosRef
           .document(user.userId)
           .collection(POSTS_FAVORITOS_USUARIO_SUBCOLLECTION)
-          .document(_getIdFromPost(post))
+          .document(getPostIdFromJson(post))
           .delete();
     } on Exception {
       throw ComunicationException();
@@ -79,46 +80,23 @@ class FirebasePostRepository implements PostRepository {
     }
   }
 
-  String _getIdFromPost(Map<String, dynamic> post) {
-    return post['idPropostaPolitico'] ?? post['id'];
-  }
-
   @override
   Future<Tuple2<Map<String, bool>, Map<String, bool>>> likePost(
       {String userId, String postId, String politicoId, bool isUnliked}) async {
     try {
-      final documentReference = await atividadesRef
-          .document(politicoId)
-          .collection(ATIVIDADES_POLITICO_SUBCOLLECTION)
-          .document(postId);
-      final documentSnapshot = await documentReference.get();
-      final actualLikesCount = documentSnapshot.data[QTD_CURTIDAS_FIELD] ?? 0;
-      final actualUnlikesCount =
-          documentSnapshot.data[QTD_NAO_CURTIDAS_FIELD] ?? 0;
-      await documentReference.updateData({
-        QTD_CURTIDAS_FIELD: actualLikesCount + 1,
-        QTD_NAO_CURTIDAS_FIELD: actualUnlikesCount - (isUnliked ? 1 : 0)
-      });
+      final isLiking = true;
 
-      final userDocumentReference = usersRef.document(userId);
-      final userDocumentSnapshot = await userDocumentReference.get();
-      final userLikes = Map<String, bool>.from(
-          userDocumentSnapshot.data[USER_LIKES_FIELD] ?? {});
-      final userUnlikes = Map<String, bool>.from(
-          userDocumentSnapshot.data[USER_UNLIKES_FIELD] ?? {});
-      userLikes[postId] = true;
-      userUnlikes[postId] = false;
-      await userDocumentReference.updateData({
-        USER_LIKES_FIELD: {
-          ...userLikes,
-        },
-        USER_UNLIKES_FIELD: {
-          ...userUnlikes,
-        }
-      });
-      return Tuple2<Map<String, bool>, Map<String, bool>>(
-        userLikes,
-        userUnlikes,
+      await _updateActivity(
+        postId: postId,
+        politicoId: politicoId,
+        isLiking: isLiking,
+        isUnliked: isUnliked,
+      );
+
+      return _updateUserLikesInfo(
+        postId: postId,
+        userId: userId,
+        isLiking: isLiking,
       );
     } on Exception {
       throw ComunicationException();
@@ -129,35 +107,19 @@ class FirebasePostRepository implements PostRepository {
   Future<Tuple2<Map<String, bool>, Map<String, bool>>> unlikePost(
       {String userId, String postId, String politicoId, bool isLiked}) async {
     try {
-      final documentReference = await atividadesRef
-          .document(politicoId)
-          .collection(ATIVIDADES_POLITICO_SUBCOLLECTION)
-          .document(postId);
-      final documentSnapshot = await documentReference.get();
-      final actualLikesCount = documentSnapshot.data[QTD_CURTIDAS_FIELD] ?? 0;
-      final actualUnlikesCount =
-          documentSnapshot.data[QTD_NAO_CURTIDAS_FIELD] ?? 0;
-      await documentReference.updateData({
-        QTD_CURTIDAS_FIELD: actualLikesCount - (isLiked ? 1 : 0),
-        QTD_NAO_CURTIDAS_FIELD: actualUnlikesCount + 1,
-      });
+      final isLiking = false;
 
-      final userDocumentReference = usersRef.document(userId);
-      final userDocumentSnapshot = await userDocumentReference.get();
-      final userLikes = Map<String, bool>.from(
-          userDocumentSnapshot.data[USER_LIKES_FIELD] ?? {});
-      final userUnlikes = Map<String, bool>.from(
-          userDocumentSnapshot.data[USER_UNLIKES_FIELD] ?? {});
-      userLikes[postId] = false;
-      userUnlikes[postId] = true;
+      await _updateActivity(
+        postId: postId,
+        politicoId: politicoId,
+        isLiking: isLiking,
+        isLiked: isLiked,
+      );
 
-      await userDocumentReference.updateData({
-        USER_LIKES_FIELD: userLikes,
-        USER_UNLIKES_FIELD: userUnlikes,
-      });
-      return Tuple2<Map<String, bool>, Map<String, bool>>(
-        userLikes,
-        userUnlikes,
+      return _updateUserLikesInfo(
+        postId: postId,
+        userId: userId,
+        isLiking: isLiking,
       );
     } on Exception {
       throw ComunicationException();
@@ -168,27 +130,19 @@ class FirebasePostRepository implements PostRepository {
   Future<Map<String, bool>> stopLikingPost(
       {String userId, String postId, String politicoId}) async {
     try {
-      final documentReference = await atividadesRef
-          .document(politicoId)
-          .collection(ATIVIDADES_POLITICO_SUBCOLLECTION)
-          .document(postId);
-      final documentSnapshot = await documentReference.get();
-      final actualLikesCount = documentSnapshot.data[QTD_CURTIDAS_FIELD] ?? 0;
-      await documentReference.updateData({
-        QTD_CURTIDAS_FIELD: actualLikesCount - 1,
-      });
+      final isLiking = true;
 
-      final userDocumentReference = usersRef.document(userId);
-      final userDocumentSnapshot = await userDocumentReference.get();
-      final userLikes = Map<String, bool>.from(
-          userDocumentSnapshot.data[USER_LIKES_FIELD] ?? {});
-      userLikes[postId] = false;
-      await userDocumentReference.updateData({
-        USER_LIKES_FIELD: {
-          ...userLikes,
-        }
-      });
-      return userLikes;
+      await _updateActivityWithStop(
+        postId: postId,
+        politicoId: politicoId,
+        isLiking: isLiking,
+      );
+
+      return _updateUserLikesInfoWhenStop(
+        postId: postId,
+        userId: userId,
+        isLiking: isLiking,
+      );
     } on Exception {
       throw ComunicationException();
     }
@@ -198,27 +152,158 @@ class FirebasePostRepository implements PostRepository {
   Future<Map<String, bool>> stopUnlikingPost(
       {String userId, String postId, String politicoId}) async {
     try {
+      final isLiking = false;
+
+      await _updateActivityWithStop(
+        postId: postId,
+        politicoId: politicoId,
+        isLiking: isLiking,
+      );
+
+      return _updateUserLikesInfoWhenStop(
+        postId: postId,
+        userId: userId,
+        isLiking: isLiking,
+      );
+    } on Exception {
+      throw ComunicationException();
+    }
+  }
+
+  Future<void> _updateActivity({
+    String postId,
+    String politicoId,
+    bool isLiking,
+    bool isLiked,
+    bool isUnliked,
+  }) async {
+    final postInfo = await _getPostInfo(postId: postId, politicoId: politicoId);
+
+    final postData = postInfo.item1;
+    final actualLikesCount = postData[QTD_CURTIDAS_FIELD] ?? 0;
+    final actualUnlikesCount = postData[QTD_NAO_CURTIDAS_FIELD] ?? 0;
+
+    final docPost = postInfo.item2;
+    if (isLiking) {
+      await docPost.updateData({
+        QTD_CURTIDAS_FIELD: actualLikesCount + 1,
+        QTD_NAO_CURTIDAS_FIELD: actualUnlikesCount - (isUnliked ? 1 : 0)
+      });
+    } else {
+      await docPost.updateData({
+        QTD_CURTIDAS_FIELD: actualLikesCount - (isLiked ? 1 : 0),
+        QTD_NAO_CURTIDAS_FIELD: actualUnlikesCount + 1,
+      });
+    }
+  }
+
+  Future<void> _updateActivityWithStop({
+    String postId,
+    String politicoId,
+    bool isLiking,
+  }) async {
+    final postInfo = await _getPostInfo(postId: postId, politicoId: politicoId);
+
+    final postData = postInfo.item1;
+    final postDoc = postInfo.item2;
+
+    if (isLiking) {
+      final actualLikesCount = postData[QTD_CURTIDAS_FIELD] ?? 0;
+      await postDoc.updateData({
+        QTD_CURTIDAS_FIELD: actualLikesCount - 1,
+      });
+    } else {
+      final actualUnlikesCount = postData[QTD_NAO_CURTIDAS_FIELD] ?? 0;
+      await postDoc.updateData({
+        QTD_NAO_CURTIDAS_FIELD: actualUnlikesCount - 1,
+      });
+    }
+  }
+
+  Future<Tuple2<Map<String, bool>, Map<String, bool>>> _updateUserLikesInfo({
+    String userId,
+    String postId,
+    bool isLiking,
+  }) async {
+    final userInfo = await _getUserInfo(userId: userId);
+
+    final userData = userInfo.item1;
+    final userLikes = Map<String, bool>.from(userData[USER_LIKES_FIELD] ?? {});
+    final userUnlikes =
+        Map<String, bool>.from(userData[USER_UNLIKES_FIELD] ?? {});
+    userLikes[postId] = isLiking;
+    userUnlikes[postId] = !isLiking;
+
+    final userDoc = userInfo.item2;
+    await userDoc.updateData({
+      USER_LIKES_FIELD: {
+        ...userLikes,
+      },
+      USER_UNLIKES_FIELD: {
+        ...userUnlikes,
+      }
+    });
+
+    return Tuple2<Map<String, bool>, Map<String, bool>>(
+      userLikes,
+      userUnlikes,
+    );
+  }
+
+  Future<Map<String, bool>> _updateUserLikesInfoWhenStop({
+    String userId,
+    String postId,
+    bool isLiking,
+  }) async {
+    final userInfo = await _getUserInfo(userId: userId);
+    final userData = userInfo.item1;
+    final userDoc = userInfo.item2;
+
+    if (isLiking) {
+      final userLikes =
+          Map<String, bool>.from(userData[USER_LIKES_FIELD] ?? {});
+      userLikes[postId] = false;
+      await userDoc.updateData({
+        USER_LIKES_FIELD: {
+          ...userLikes,
+        }
+      });
+      return userLikes;
+    } else {
+      final userUnlikes =
+          Map<String, bool>.from(userData[USER_UNLIKES_FIELD] ?? {});
+      userUnlikes[postId] = false;
+
+      await userDoc.updateData({
+        USER_UNLIKES_FIELD: userUnlikes,
+      });
+      return userUnlikes;
+    }
+  }
+
+  Future<Tuple2<Map<String, dynamic>, DocumentReference>> _getPostInfo({
+    String politicoId,
+    String postId,
+  }) async {
+    try {
       final documentReference = await atividadesRef
           .document(politicoId)
           .collection(ATIVIDADES_POLITICO_SUBCOLLECTION)
           .document(postId);
       final documentSnapshot = await documentReference.get();
-      final actualUnlikesCount =
-          documentSnapshot.data[QTD_NAO_CURTIDAS_FIELD] ?? 0;
-      await documentReference.updateData({
-        QTD_NAO_CURTIDAS_FIELD: actualUnlikesCount - 1,
-      });
+      return Tuple2(documentSnapshot.data, documentReference);
+    } on Exception {
+      throw ComunicationException();
+    }
+  }
 
+  Future<Tuple2<Map<String, dynamic>, DocumentReference>> _getUserInfo({
+    String userId,
+  }) async {
+    try {
       final userDocumentReference = usersRef.document(userId);
       final userDocumentSnapshot = await userDocumentReference.get();
-      final userUnlikes = Map<String, bool>.from(
-          userDocumentSnapshot.data[USER_UNLIKES_FIELD] ?? {});
-      userUnlikes[postId] = false;
-
-      await userDocumentReference.updateData({
-        USER_UNLIKES_FIELD: userUnlikes,
-      });
-      return userUnlikes;
+      return Tuple2(userDocumentSnapshot.data, userDocumentReference);
     } on Exception {
       throw ComunicationException();
     }
