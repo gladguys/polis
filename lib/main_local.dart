63cadde13d0554233f2153e71cd725c1f2ddf818
Rodrigo <rodrigobastosv@gmail.com>
2020-07-08 23:54:06 -0300

    adjusts

--- lib/main_local.dart
+++ lib/main_local.dart
@@ -3,6 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'main_commom.dart' as commom;
 
 void main() async {
-  await DotEnv().load('env/.env_local');
+  await DotEnv().load('.env_local');
   commom.main();
 }
