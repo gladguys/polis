774d3470302450c43776a1591a0f9a7660cda643
rodrigobastosv <rodrigobastosv@gmail.com>
2020-07-22 13:47:21 -0300

    fixing tests

--- test/core/domain/model/comment_model_test.dart
+++ test/core/domain/model/comment_model_test.dart
@@ -30,7 +30,7 @@ void main() {
     test('toString()', () {
       final modelToString = CommentModel(id: 1).toString();
       final result =
-          '''CommentModel{id: 1, usuarioId: null, usuarioNome: null, postId: null, texto: null, diaHora: null, foiEditado: null, qntSubComentarios: null}''';
+          '''CommentModel{id: 1, usuarioId: null, usuarioNome: null, postId: null, politicoId: null, texto: null, diaHora: null, foiEditado: null, qntSubComentarios: null}''';
       expect(modelToString == result, true);
     });
 
